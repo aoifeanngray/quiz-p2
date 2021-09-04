edaececcba7f6740c6a767a2a962b3badbf4a4f6
aoifeanngray <aoifeanngray@hotmail.com>
2021-09-04 08:46:23 +0000

    Corrected JS errors

--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,91 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainer = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
 let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
-  setNextQuestion()
+  setNextQuestion();
 })
 // begin game: 
 function startGame () {
-    console.log('started')
-    startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(()=> Math.random() -.5)
-    currentQuestionIndex = 0
-    questionContainer.classList.remove('hide')
-    setNextQuestion()
+    console.log('started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(()=> Math.random() -.5);
+    currentQuestionIndex = 0;
+    questionContainer.classList.remove('hide');
+    setNextQuestion();
 }
 function setNextQuestion () {
-  resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 // make question appear:
 function showQuestion (question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question;
     question.answers.forEach(answer => {
       const button =document.createElement('button')
-      button.innerText = answer.text
-      button.classList.add('btn')
+      button.innerText = answer.text;
+      button.classList.add('btn');
       if (answer.correct) {
-        button.dataset.correct = answer.correct
+        button.dataset.correct = answer.correct;
       }
-    button.addEventListener('click', selectAnswer) 
-    answerButtonsElement.appendChild(button)
+    button.addEventListener('click', selectAnswer); 
+    answerButtonsElement.appendChild(button);
       })
     }
 // reset game: 
     function resetState() {
-      clearStatusClass(document.body)
-      nextButton.classList.add('hide')
-      while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+      clearStatusClass(document.body);
+      nextButton.classList.add('hide');
+      while (answerButtonsElement.firstChild); {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
       }
     }
 // select answers :
 function selectAnswer (e) {
-  const selectedButton = e.target 
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach(button => {
-  setStatusClass(button, button.dataset.correct)
+  setStatusClass(button, button.dataset.correct);
   })
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    startButton.innerText = ('Restart');
+    startButton.classList.remove('hide');
   }
 }
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add('correct');
   } else {
-    element.classList.add('wrong')
+    element.classList.add('wrong');
   }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
 function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add('correct');
   } else { 
-    element.classList.add('wrong')
+    element.classList.add('wrong');
   }
 }
 
   function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
   }
 
   // Questions and answers list: 
